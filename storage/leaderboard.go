b1f145c4938abd19caa14788f2d95d34b3d05ca4
alex <norther48@list.ru>
2018-05-28 17:07:15 +0300

    Refactored leaderoards queries

--- storage/leaderboard.go
+++ storage/leaderboard.go
@@ -14,21 +14,43 @@ func (u User) Leaderboard(lbType string, lbName string) (string, error) {
 		return "", errors.New("databases not initialized")
 	}
 	if lbType == "all" {
-		q := fmt.Sprintf("SET @row_num = 0; SELECT CAST(CONCAT('[', GROUP_CONCAT(CONCAT('{', '\"pos\":',  k.`num`, ',' ,'\"name\":', JSON_QUOTE(k.`name`), ',', '\"score\":',	k.`%s`, '}')),']') AS JSON) "+
-			"FROM (SELECT p.`num`, p.`name`, p.`%s` "+
-			"FROM (SELECT (@row_num := @row_num +1) As num, u.`name`,u.`%s`, u.`id` FROM `users` u  ORDER BY u.`%s` DESC) p "+
-			"WHERE p.`num`<= 100 OR p.`id` = %d) k ", lbName, lbName, lbName, lbName, u.ID())
-		fmt.Println(q)
+		q := fmt.Sprintf("SELECT JSON_OBJECT("+
+			"\"id\"       ,  k.`id`   , "+
+			"\"position\" ,  k.`pos`  , "+
+			"\"name\"     ,  k.`name` , "+
+			"\"score\"    ,  k.`score`, "+
+			"\"top100\"   , "+
+			"CAST(CONCAT('[',GROUP_CONCAT(DISTINCT CONCAT('{',"+
+			"'\"id\":'    , 	b.`id`, 				','  ,"+
+			"'\"name\":'  , 	JSON_QUOTE(b.`name`),   ','  ,"+
+			"'\"score\":' ,  	b.`%s`						 ,"+
+			"'}')),']') AS JSON))"+
+			"FROM(SELECT s.`id`, count(*) + 1 as pos, s.`name`, s.`score` from leader_board1 l, (select id, %s as score, name from leader_board1 o where o.`id` = %d) s "+
+			"where l.`%s` IS NOT NULL AND  l.`%s` > s.`score` ) k,"+
+			"(SELECT l.`id`, l.`name`, l.`%s` FROM leader_board1 l LIMIT 100) b GROUP BY k.`id`", lbName, lbName, u.ID(), lbName, lbName, lbName)
 		err = u.dbs.mySQL.QueryRow(q).Scan(&lb)
 	} else if lbType == "friends" {
-		q := fmt.Sprintf("SELECT CAST(CONCAT('[', GROUP_CONCAT(CONCAT('{', '\"name\":', JSON_QUOTE(p.`name`), ',', '\"score\":',	p.`%s`, '}')),']') AS JSON) "+
-			"FROM ( SELECT v.`name`, v.`%s` "+
-			"FROM (SELECT u.`name`,u.`%s` FROM `friends` f, `users` u WHERE f.`user_id2` = %d AND f.`user_id1` = u.`id` "+
+		q := fmt.Sprintf("SELECT JSON_OBJECT("+
+			"\"id\"			,  k.`id`		, "+
+			"\"position\"	,  k.`pos`		, "+
+			"\"name\"		,  k.`name`		, "+
+			"\"score\"		,  k.`score`	, "+
+			"\"friends\"	,"+
+			"CAST(CONCAT('[',GROUP_CONCAT(DISTINCT CONCAT('{',"+
+			"'\"id\":'	  , 	p.`id`, 				',' ,"+
+			"'\"name\":'  , 	JSON_QUOTE(p.`name`),   ',' ,"+
+			"'\"score\":' , 	p.`%s`,"+
+			"'}')),']') AS JSON))"+
+			"FROM ( SELECT v.`id`, v.`name`, v.`%s`"+
+			"FROM (SELECT u.`id`,u.`name`,u.`%s` FROM `friends` f, `users` u WHERE f.`user_id2` = %d AND f.`user_id1` = u.`id` "+
+			"UNION SELECT u.`id`,u.`name`,u.`%s` FROM `friends` f, `users` u WHERE f.`user_id1` = %d AND f.`user_id2` = u.`id`) v "+
+			"ORDER BY v.`%s`) p, "+
+			"(SELECT m.`id`, count(*) + 1 as pos, m.`name`, m.score "+
+			"FROM (SELECT  l.`id`, l.`name`, l.`%s` as score FROM leader_board1 l WHERE l.`id` = %d) m, "+
+			"(SELECT u.`%s` as score FROM `friends` f, `users` u WHERE f.`user_id2` = %d AND f.`user_id1` = u.`id` "+
 			"UNION "+
-			"SELECT u.`name`,u.`%s` FROM `friends` f, `users` u WHERE f.`user_id1` = %d AND f.`user_id2` = u.`id` "+
-			"UNION "+
-			"SELECT u.`name`, u.`%s` FROM `users` u WHERE u.`id` = %d) v "+
-			"ORDER BY v.`lb1` DESC LIMIT 100) p", lbName, lbName, lbName, u.ID(), lbName, u.ID(), lbName, u.ID())
+			"SELECT u.`%s` as score FROM `friends` f, `users` u WHERE f.`user_id1` = %d AND f.`user_id2` = u.`id`) s "+
+			"where m.score IS NOT NULL AND  s.score > m.score ) k GROUP BY k.`id`", lbName, lbName, lbName, u.ID(), lbName, u.ID(), lbName, lbName, u.ID(), lbName, u.ID(), lbName, u.ID())
 		err = u.dbs.mySQL.QueryRow(q).Scan(&lb)
 	}
 	if err != nil {
