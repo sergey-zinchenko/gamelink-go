3728b661dac8b37f8b5067beee9b1535bc4095dd
alex <norther48@list.ru>
2018-10-02 18:00:25 +0300

    make update with transaction; moved it from grpcFunc to storage

--- storage/admin.go
+++ storage/admin.go
@@ -6,32 +6,39 @@ import (
 	"fmt"
 	C "gamelink-go/common"
 	"gamelink-go/proto_msg"
-	"gamelink-go/storage/queries"
+)
+
+const (
+	//QueryCount - const for count query
+	QueryCount QueryKind = iota
+	//QuerySelect - const for Select query
+	QuerySelect
+	//QueryUpdate - const for update query
+	QueryUpdate
+	//QueryDelete - const for delete query
+	QueryDelete
+
+	limit = 1
 )
 
 type (
 	//QueryBuilder - struct fo work with params and build db query
 	QueryBuilder struct {
-		query, whereClause, message string
-		offset                      int64
-		params                      []interface{}
-	}
-	//UpdateBuilder - struct for work with params when update user data
-	UpdateBuilder struct {
-		ID, Count  int64
-		Data       []byte
-		UpdParams  []*proto_msg.UpdateCriteriaStruct
-		FindParams []*proto_msg.OneCriteriaStruct
-		QBuilder   QueryBuilder
+		kind        QueryKind
+		whereClause string
+		params      []interface{}
+		updParams   []*proto_msg.UpdateCriteriaStruct
 	}
 	//ScanFunc - func for scan rows
 	ScanFunc = func(...interface{}) error
 	//RowWorker - use scanfunc, return query executing result
 	RowWorker = func(ScanFunc) (interface{}, error)
+	//QueryKind - num of kind db query
+	QueryKind = int64
 )
 
 //WithClause - func for make query part from criteria
-func (q *QueryBuilder) WithClause(criteria *proto_msg.OneCriteriaStruct) *QueryBuilder {
+func (q *QueryBuilder) WithClause(criteria *proto_msg.OneCriteriaStruct) {
 	if q.whereClause != "" {
 		q.whereClause += " AND "
 	}
@@ -49,175 +56,161 @@ func (q *QueryBuilder) WithClause(criteria *proto_msg.OneCriteriaStruct) *QueryB
 		q.whereClause += ">= ?"
 	}
 	q.params = append(q.params, criteria.Value)
-	return q
 }
 
 //WithMultipleClause - loop from array of criterias
-func (q *QueryBuilder) WithMultipleClause(criterias []*proto_msg.OneCriteriaStruct) *QueryBuilder {
+func (q QueryBuilder) WithMultipleClause(criterias []*proto_msg.OneCriteriaStruct) QueryBuilder {
 	for _, v := range criterias {
-		if v.Cr.String() == "message" {
-			q.message = v.Value //Пишем в структуру сообщение на случай, если это запрос на отправку пуш сообщения
-			continue
-		}
 		q.WithClause(v)
 	}
 	return q
 }
 
-//Offset - set offset to queryBuilder
-func (q *QueryBuilder) Offset(offset int64) *QueryBuilder {
-	q.offset = offset
+//WithData - add update params to UpdateBuilder
+func (q QueryBuilder) WithData(criterias []*proto_msg.UpdateCriteriaStruct) QueryBuilder {
+	q.updParams = criterias
 	return q
 }
 
 //CountQuery - fist part of count query
-func (q *QueryBuilder) CountQuery() *QueryBuilder {
-	q.query = "SELECT COUNT(id) FROM users"
+func (q QueryBuilder) CountQuery() QueryBuilder {
+	q.kind = QueryCount
 	return q
 }
 
 //SelectQuery - first part of select query
-func (q *QueryBuilder) SelectQuery() *QueryBuilder {
-	q.query = `SELECT id, vk_id, fb_id, name, email, sex, timestampdiff(YEAR, bdate, curdate()), country, date(created_at), deleted from users`
+func (q QueryBuilder) SelectQuery() QueryBuilder {
+	q.kind = QuerySelect
+	return q
+}
+
+//UpdateQuery - query for update command
+func (q QueryBuilder) UpdateQuery() QueryBuilder {
+	q.kind = QueryUpdate
 	return q
 }
 
 //DeleteQuery - first part of delete query
-func (q *QueryBuilder) DeleteQuery() *QueryBuilder {
-	q.query = `UPDATE users SET deleted=1`
+func (q QueryBuilder) DeleteQuery() QueryBuilder {
+	q.kind = QueryDelete
 	return q
-}
-
-//PushQuery - first part of query for find users who will recieve push message
-func (q *QueryBuilder) PushQuery() *QueryBuilder {
-	q.query = `**********************`
-	return q
-}
-
-//GetData - get user data from db
-func (q *QueryBuilder) GetData() *QueryBuilder {
-	q.query = `SELECT id, data FROM users `
-	return q
-}
-
-func (q *QueryBuilder) isGetDataQuery() bool {
-	if q.query == `SELECT id, data FROM users ` {
-		return true
-	}
-	return false
-}
-
-//Concat - concatenate first query part, WHERE and params query part
-func (q *QueryBuilder) Concat(offset int64) string {
-	if q.query == "" {
-		return ""
-	}
-	if q.whereClause == "" {
-		return q.query
-	}
-	query := fmt.Sprintf("%s WHERE %s", q.query, q.whereClause)
-	if q.isGetDataQuery() {
-		query += fmt.Sprintf(" LIMIT 1 OFFSET %d", offset)
-	}
-	return query
 }
 
 //QueryWithDB - execute query, scan result
-func (q *QueryBuilder) QueryWithDB(sql *sql.DB, worker RowWorker) ([]interface{}, error) {
-	rows, err := sql.Query(q.Concat(q.offset), q.params...)
-	defer rows.Close()
-	if err != nil {
-		return nil, err
-	}
-	var res []interface{}
-	for rows.Next() {
-		oneres, err := worker(rows.Scan)
+func (q QueryBuilder) QueryWithDB(mysql *sql.DB, worker RowWorker) ([]interface{}, error) {
+	switch q.kind {
+	case QuerySelect, QueryCount, QueryDelete:
+		var query string
+		switch q.kind {
+		case QueryCount:
+			query = `SELECT COUNT(id) FROM users`
+		case QuerySelect:
+			query = `SELECT id, vk_id, fb_id, name, email, sex, timestampdiff(YEAR, bdate, curdate()), country, date(created_at), deleted from users`
+		case QueryDelete:
+			query = `UPDATE users SET deleted=1`
+		}
+		if q.whereClause != "" {
+			query = fmt.Sprintf("%s WHERE %s", query, q.whereClause)
+		}
+		rows, err := mysql.Query(query, q.params...)
+		defer rows.Close()
 		if err != nil {
 			return nil, err
 		}
-		res = append(res, oneres)
+		var res []interface{}
+		for rows.Next() {
+			oneres, err := worker(rows.Scan)
+			if err != nil {
+				return nil, err
+			}
+			res = append(res, oneres)
+		}
+		return res, nil
+	case QueryUpdate:
+		tx, err := mysql.Begin()
+		if err != nil {
+			return nil, err
+		}
+		err = updateTransaction(q.whereClause, q.updParams, q.params, tx)
+		if err != nil {
+			if e := tx.Rollback(); e != nil {
+				return nil, e
+			}
+			return nil, err
+		}
+		err = tx.Commit()
+		if err != nil {
+			return nil, err
+		}
 	}
-	return res, nil
-}
-
-//Message - return query builder message
-func (q *QueryBuilder) Message() string {
-	return q.message
+	return nil, nil
 }
 
 //Prepare - get user json, update fields, adn prepare to update it in db
-func (u *UpdateBuilder) Prepare() (*UpdateBuilder, error) {
+func prepareDataToUpdateInDb(rowData []byte, updateParams []*proto_msg.UpdateCriteriaStruct) ([]byte, error) {
 	var dataJSON C.J
-	err := json.Unmarshal(u.Data, &dataJSON)
+	err := json.Unmarshal(rowData, &dataJSON)
 	if err != nil {
 		return nil, err
 	}
-	for _, v := range u.UpdParams {
+	for _, v := range updateParams {
 		if v.Uop == proto_msg.UpdateCriteriaStruct_set {
 			dataJSON[v.Ucr.String()] = v.Value
 		} else if v.Uop == proto_msg.UpdateCriteriaStruct_delete {
 			delete(dataJSON, v.Ucr.String())
 		}
 	}
-	u.Data, err = json.Marshal(dataJSON)
+	fmt.Println(dataJSON)
+	newData, err := json.Marshal(dataJSON)
 	if err != nil {
 		return nil, err
 	}
-	return u, nil
+	return newData, nil
 }
 
-// UpdateWithDb - execute db query
-func (u *UpdateBuilder) UpdateWithDb(mysql *sql.DB) error {
-	//_, err := sql.Exec(queries.AdminUpdateUserDataQuery, u.Data, u.ID)
-	//if err != nil {
-	//	return err
-	//}
-	//return nil
-	var transaction = func(tx *sql.Tx) error {
-		var i int64
-		for i = 0; i < u.Count; i = i + 1 {
-			u.QBuilder.Offset(i)
-			u.QBuilder.GetData().WithMultipleClause(u.FindParams)
-			_, err := u.QBuilder.QueryWithDB(mysql, func(scanFunc ScanFunc) (interface{}, error) {
-				var bytes []byte
-				var ident int64
-				err := scanFunc(&ident, &bytes)
-				if err != nil {
-					return nil, err
-				}
-				u.ID = ident
-				u.Data = bytes
-				updated, err := u.Prepare()
-				if err != nil {
-					return nil, err
-				}
-				_, err = mysql.Exec(queries.AdminUpdateUserDataQuery, updated.Data, updated.ID)
-				if err != nil {
-					return nil, err
-				}
-				return nil, nil
-			})
+func updateTransaction(whereClause string, updParams []*proto_msg.UpdateCriteriaStruct, params []interface{}, tx *sql.Tx) error {
+	var offset int64
+	params = append(params, limit)
+	params = append(params, offset)
+	for {
+		var count int64
+		params[len(params)-1] = offset
+		query := "SELECT id, data FROM gamelink.users WHERE " + whereClause + " LIMIT ? OFFSET ?"
+		rows, err := tx.Query(query, params...)
+		//defer rows.Close()
+		if err != nil {
+			if err == sql.ErrNoRows {
+				break
+			}
+			return err
+		}
+		type update struct {
+			id   int64
+			data []byte
+		}
+		var updateSet []update
+		for rows.Next() {
+			count++
+			var id int64
+			var oldData []byte
+			rows.Scan(&id, &oldData)
+			newData, err := prepareDataToUpdateInDb(oldData, updParams)
+			if err != nil {
+				return err
+			}
+			upd := update{id: id, data: newData}
+			updateSet = append(updateSet, upd)
+		}
+		for _, v := range updateSet {
+			_, err = tx.Exec("UPDATE users set data = ? WHERE id = ?", v.data, v.id)
 			if err != nil {
 				return err
 			}
 		}
-		return nil
-	}
-
-	tx, err := mysql.Begin()
-	if err != nil {
-		return err
-	}
-	err = transaction(tx)
-	if err != nil {
-		if e := tx.Rollback(); e != nil {
-			return e
+		if count < limit {
+			break
 		}
-		return err
-	}
-	err = tx.Commit()
-	if err != nil {
-		return err
+		offset += limit
 	}
 	return nil
 }
