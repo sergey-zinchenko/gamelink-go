9ed352967be50ad04aac6edf4b80b4d1f383ff07
alex <norther48@list.ru>
2018-12-06 11:35:52 +0400

    change version

--- build.sh
+++ build.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-#PROJECT=registry.gitlab.smedialink.com/z/gamelink-go
 PROJECT=gamelink-go
-RELEASE=1.0
+RELEASE=1.0.1
 COMMIT=`git rev-parse --short HEAD`
 BUILD_TIME=$(date -u '+%Y-%m-%d_%H:%M:%S')
 env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -ldflags "-X ${PROJECT}/version.Release=${RELEASE} -X ${PROJECT}/version.Commit=${COMMIT} -X ${PROJECT}/version.BuildTime=${BUILD_TIME}"
-#docker build -t registry.gitlab.smedialink.com/z/gamelink-go .
 docker build -t mrcarrot/gamelink-go .
-#docker push registry.gitlab.smedialink.com/z/gamelink-go
 docker push mrcarrot/gamelink-go
