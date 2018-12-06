7477483e00306fe4905d7118f31cb8f0ac92ae37
alex <norther48@list.ru>
2018-12-06 11:44:50 +0400

    change version in deployment

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ RELEASE=1.0.1
 COMMIT=`git rev-parse --short HEAD`
 BUILD_TIME=$(date -u '+%Y-%m-%d_%H:%M:%S')
 env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -ldflags "-X ${PROJECT}/version.Release=${RELEASE} -X ${PROJECT}/version.Commit=${COMMIT} -X ${PROJECT}/version.BuildTime=${BUILD_TIME}"
-docker build -t mrcarrot/gamelink-go .
+docker build -t mrcarrot/gamelink-go:1.0.1 .
 docker push mrcarrot/gamelink-go
