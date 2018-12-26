14c6a5def254633d65de604edf8c1edd9f8b3e6d
alex <norther48@list.ru>
2018-12-26 18:01:05 +0400

    change error when token doesn't exist in redis

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 PROJECT=gamelink-go
-RELEASE=1.0.1
+RELEASE=1.0.2
 COMMIT=`git rev-parse --short HEAD`
 BUILD_TIME=$(date -u '+%Y-%m-%d_%H:%M:%S')
 env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -ldflags "-X ${PROJECT}/version.Release=${RELEASE} -X ${PROJECT}/version.Commit=${COMMIT} -X ${PROJECT}/version.BuildTime=${BUILD_TIME}"
